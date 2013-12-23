0a67344522cfdcfc42d1f1a16fc60d95a104cf2d
Uladzimir Kazakevich <kwull@kwull.com>
2013-12-23 15:54:36 +0300

    Fix original boxstarter

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -1,6 +1,4 @@
-Set-ExecutionPolicy Unrestricted
 Disable-UAC
-Enable-RemoteDesktop
 Disable-InternetExplorerESC
 Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 Move-LibraryDirectory "{374DE290-123F-4565-9164-39C4925E467B}" "d:\Downloads"
@@ -59,51 +57,30 @@ cinst ScriptCs
 
 cinst truecrypt
 
-Set-ExecutionPolicy RemoteSigned
 Write-Host "Installation completed"
 
-# Create Powershell profile if it's not exist
-Write-Host "Looking for user profile"
-If ((Test-Path $profile) -eq $True) {
-    Write-Host "Profile exists"
-}
-Else {
-    Write-Host "Profile doesn't exist. Trying to create..."
-    New-Item -Path $profile -ItemType File -force
-    If ((Test-Path $profile) -eq $True) {
-        Write-Host "Profile was successfuly created"
-    }
-    Else {
-        Write-Host "Can not create profile. Exiting.."
-        Exit
-    }
-}
-# Configure Powershell
-Write-Host "Setting up Powershell start location"
-"Set-Location C:\" >> $profile
-
-# Configure Git
-Write-Host "Adding Git bin to PATH"
-'$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"' >> $profile
-$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
-
-# Configure PoshGit
-Write-Host "Adding PoshGit to PATH"
-'$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\cmd"' >> $profile
-$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\cmd"
-
 git config --global user.name "Uladzimir Kazakevich"
-git config --global user.email "kwull@kwull.com"
 git config --global push.default simple
 git config --global credential.helper store
+git config --global alias.a add
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.d diff
+git config --global alias.br branch
+git config --global alias.mr merge
+git config --global alias.l log
+git config --global alias.p pull
+git config --global alias.rb rebase
+git config --global alias.sh show
 
 Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles\Paint.NET\PaintDotNet.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\Fiddler2\Fiddler.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\LINQPad4\LINQPad.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\Remote Desktop Connection Manager\RDCMan.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\XMind\XMind.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\AIMP3\AIMP3.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\Google\Chrome\Application\chrome.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\AIMP3\AIMP3.exe"
-Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles(x86)\AIMP3\AIMP3.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Fiddler2\Fiddler.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\LINQPad4\LINQPad.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Remote Desktop Connection Manager\RDCMan.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\XMind\XMind.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\AIMP3\AIMP3.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\AIMP3\AIMP3.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\AIMP3\AIMP3.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\totalcmd\TOTALCMD64.EXE"

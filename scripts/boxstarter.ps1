8ecb7add12f105fd9cc50bf9a9a64e855df68a10
Uladzimir Kazakevich <kwull@kwull.com>
2016-04-15 12:45:53 +0300

    Update boxstarter.ps1

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -3,16 +3,12 @@
 Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 #Move-LibraryDirectory "{374DE290-123F-4565-9164-39C4925E467B}" "c:\!Work\Downloads"
 
-cinst TelnetClient -source windowsfeatures
-#cinst Microsoft-Hyper-V-All -source windowsfeatures
-
-#cinst webpicommandline
-
 #editors and viewers
 choco install notepadplusplus.install -y
 choco install paint.net -y
 choco install fsviewer -y
 choco install adobereader -y
+#choco install xmind -y
 
 #web 
 choco install googlechrome -y
@@ -20,6 +16,10 @@ choco install jre8 -y
 choco install flashplayerplugin -y
 
 #dev tools
+choco install resharper-platform -y
+### resharper ultimate download only
+### cd c:\ProgramData\chocolatey\lib\resharper-platform
+choco install linqpad -y
 choco install fiddler4 -y
 choco install git.install -y
 choco install poshgit -y
@@ -33,6 +33,7 @@ choco install skype -y
 choco install telegram.install -y
 
 #tools
+choco install TotalCommander -y
 choco install dropbox -y
 choco install sysinternals -y
 choco install putty -y
@@ -42,29 +43,21 @@ choco install curl -y
 choco install wget -y
 choco install windirstat -y
 choco install TeraCopy -y
+choco install f.lux -y
 
 ########
-# choco install docker -y
-choco install openssl.light -y
-
-#cinst spotify
-
-cinst TotalCommander
-#cinst freefilesync
-cinst f.lux
-cinst gow
-#cinst xmind
-
+# outdated a bit
+#choco install docker -y
+#choco install openssl.light -y
+#cinst gow
 #cinst winmerge
-cinst linqpad4
-cinst dotPeek
-cinst windbg
+#cinst windbg
 #cinst baretail
-cinst AzureStorageExplorer
+#cinst AzureStorageExplorer
 
 Write-Host "Installation completed"
 
-#git config --global user.name "Uladzimir Kazakevich"
+git config --global user.name "Uladzimir Kazakevich"
 git config --global core.autocrlf false
 git config --global push.default simple
 git config --global credential.helper store
@@ -91,9 +84,6 @@ Install-ChocolateyPinnedTaskBarItem "$env:ProgramFiles\Paint.NET\PaintDotNet.exe
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Fiddler2\Fiddler.exe"
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\LINQPad4\LINQPad.exe"
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Neudesic\Azure Storage Explorer\AzureStorageExplorer.exe"
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Remote Desktop Connection Manager\RDCMan.exe"
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\XMind\XMind.exe"
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\AIMP3\AIMP3.exe"
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\AIMP3\AIMP3.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\totalcmd\TOTALCMD64.EXE"
